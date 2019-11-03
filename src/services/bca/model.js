99082c8de5422e69ebb7fabc06c5ff63a8b64b13
hendra sadewa <hendra.sadewa@hara.ag>
2019-11-03 22:17:05 +0700

    remove created / updated at

--- src/services/bca/model.js
+++ src/services/bca/model.js
@@ -5,9 +5,7 @@ const userBcaSchema = new mongoose.Schema(
     username: { type: String, required: true, trim: true },
     pin: { type: String, required: true },
     account_number: { type: String, required: true, unique: true },
-    balance: { type: Number, required: true, default: 100000.0 },
-    created_at: Date,
-    updated_at: Date
+    balance: { type: Number, required: true, default: 100000.0 }
   },
   {
     timestamps: {
